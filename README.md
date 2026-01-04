eff91ffdbe25456a0794a68bcb74b3f4150cdbc3
LouisXVI07 <anablaptop@gmail.com>
2026-01-04 22:12:01 +0530

    first push

---
+++ README.md
@@ -0,0 +1,3 @@
+This repo contains all the cf porblems i willsolve 5th of jan 2026 onwards 
+I will see if i add my previous solution, although they arent much
+The name of the file is the porblem id
---
+++ jan 2026/189A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int a;
+    cin>>a;
+    for (int i=0;i<a;i++){
+        cout<<"Nothing to see here"<<'\n';
+    }
+
+    
+}
+
+int main(){
+    
+    int t=1; 
+
+    //cin >> t;
+    
+    while(t--){
+        solve();
+    }
+    return 0;
+
+}
